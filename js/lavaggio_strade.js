4ee7dcaf395afe455e27766ed638363ede923cf5
Maurizio Napolitano <napo@fbk.eu>
2018-04-07 16:37:28 +0200

    update con calendario 2018

--- js/lavaggio_strade.js
+++ js/lavaggio_strade.js
@@ -4,8 +4,6 @@
         'OpenStreetMap</a> contributors';
       var mapboxUrl = 'http://{s}.tiles.mapbox.com/v3/tmcw.map-7s15q36b/{z}/{x}/{y}.png';
       var mapboxAttrib = 'Data, imagery and map information provided by <a href="http://mapbox.com" target="_blank">Mapbox</a>,<a href="http://www.openstreetmap.org/" target="_blank">OpenStreetMap</a> and contributors.';
-        var mapquestUrl = 'http://otile1.mqcdn.com/tiles/1.0.0/osm//{z}/{x}/{y}.png';
-        var mapquestAttrib = '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap contributors.</a> Tiles courtesy of <a href="http://www.mapquest.com/">MapQuest</a> <img src="https://developer.mapquest.com/content/osm/mq_logo.png">';
 
       var mapbox = L.tileLayer(mapboxUrl, {
         maxZoom: 18,
@@ -17,11 +15,6 @@
         attribution: osmAttrib,
         noWrap: true
       });
-      var mapquest = L.tileLayer(mapquestUrl, {
-        maxZoom: 18,
-        attribution: mapquestAttrib,
-        noWrap: true
-      });
       var map = L.map('map', {
         layers: [mapbox],
         center: new L.LatLng(46.0664,11.1412),
@@ -30,13 +23,6 @@
       });
       var defaultBounds = map.getBounds();
 	
-	    lc = L.control.locate({
-        		follow: true,
-        		strings: {
-            		title: "Mostrami dove mi trovo!"
-        		}
-	    }).addTo(map);
-
         map.on('startfollowing', function() {
             map.on('dragstart', lc._stopFollowing, lc);
         }).on('stopfollowing', function() {
@@ -68,10 +54,11 @@
         list.innerHTML = "";
         displayed.forEach(function(street){
         if (street.properties) {
-          if (street.properties.limit != "NaN") {
-            streetname = street.properties.name + '<br/><span style="font-style:strong;font-size:xx-small;">'+street.properties.limit+'</span>';
+          if (street.properties.note != null) {
+
+            streetname = street.properties.via + '<br/><span style="font-style:strong;font-size:xx-small;">'+street.properties.note+'</span>';
           } else {
-            streetname = street.properties.name
+            streetname = street.properties.via
           }
           if (streets.indexOf(streetname) == -1) {
             var li = document.createElement('li');
