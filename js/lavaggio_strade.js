91932a1593c764a221a3092484f861d1b921c710
Maurizio Napolitano <napo@fbk.eu>
2019-04-11 14:46:51 +0200

    sfondo facebook

--- js/lavaggio_strade.js
+++ js/lavaggio_strade.js
@@ -4,8 +4,10 @@
         'OpenStreetMap</a> contributors';
       var mapboxUrl = 'http://{s}.tiles.mapbox.com/v3/tmcw.map-7s15q36b/{z}/{x}/{y}.png';
       var mapboxUrl = 'https://api.mapbox.com/styles/v1/mapbox/streets-v10/tiles/256/{z}/{x}/{y}?access_token=pk.eyJ1IjoidG9tYXNkaWV6IiwiYSI6ImRTd01HSGsifQ.loQdtLNQ8GJkJl2LUzzxVg'
-      var mapboxAttrib = 'Data, imagery and map information provided by <a href="http://mapbox.com" target="_blank">Mapbox</a>,<a href="http://www.openstreetmap.org/" target="_blank">OpenStreetMap</a> and contributors.';
+      var mapboxUrl = 'https://external-mxp1-1.xx.fbcdn.net/map_tile.php?v=1014&osm_provider=2&x={x}&y={y}&z={z}&language=it_IT'
 
+      var mapboxAttrib = 'Data, imagery and map information provided by <a href="http://mapbox.com" target="_blank">Mapbox</a>,<a href="http://www.openstreetmap.org/" target="_blank">OpenStreetMap</a> and contributors.';
+      var mapboxAttrib = 'Data, imagery and map information provided by <a href="http://facebook.com" target="_blank">Mapbox</a>,<a href="http://www.openstreetmap.org/" target="_blank">OpenStreetMap</a> and contributors.';
       var mapbox = L.tileLayer(mapboxUrl, {
         maxZoom: 18,
         attribution: mapboxAttrib,
